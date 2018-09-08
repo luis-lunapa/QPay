a7dde2a3e50f8925ee22709db1ee941e4f87e191
luisLuna24 <luis.lunapa@outlook.com>
2018-09-07 20:05:06 -0500

    Primer commit

---
+++ QPay/QRScannerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  QRScannerViewController.swift
+//  QPay
+//
+//  Created by Luis Luna on 9/7/18.
+//  Copyright © 2018 Luis Luna. All rights reserved.
+//
+
+import UIKit
+
+class QRScannerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
