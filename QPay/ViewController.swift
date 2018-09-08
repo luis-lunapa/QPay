82508052c241487483569db245dac7aa99ceabf9
luisLuna24 <luis.lunapa@outlook.com>
2018-09-07 19:43:38 -0500

    Initial Commit

---
+++ QPay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QPay
+//
+//  Created by Luis Luna on 9/7/18.
+//  Copyright © 2018 Luis Luna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
